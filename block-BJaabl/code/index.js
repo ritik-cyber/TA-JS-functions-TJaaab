99bdef36fa536a92877fcc481f16196a4adcecc0
ritik-cyber <ritikdhingra03@gmail.com>
2021-05-21 14:44:24 +0530

    done

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,13 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+function sayHello(name) {
+  alert(`Hello ${name}`);
+}
+
+sayHello("Ritik");
+sayHello("Arya");
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,6 +17,12 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+
+function getFullName(firstName, secondName) {
+  return `${firstName + secondName}`;
+}
+getFullName("john", "snow");
+getFullName("Nelson", "Mandela");
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -21,6 +34,12 @@ addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
 
+function addTwoNumbers() {
+  let firstNum = +prompt("Enter a Number");
+  let secondNum = +prompt("Enter a Number");
+  return firstNum + secondNum;
+}
+console.log(addTwoNumbers());
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
 can only be one of these `add, sub, mul, div`. Based on the operator return the value. If the `numA` or `numB`
@@ -33,6 +52,10 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+function calc(numA, numB) {
+  console.log(`${numA + numB}`);
+}
+calc(12, 12, "+");
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -41,7 +64,10 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
-
+function isLeapYear(year) {
+  if (year === 400 || year === 4) return true;
+}
+isLeapYear(2000);
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
